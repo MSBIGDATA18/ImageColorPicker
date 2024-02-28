525f6abcf056fd6cb95537ca8d7b23a40464fc77
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2024-02-28 13:39:18 +0100

    Please check app.js, this version doesn't working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const imgInput = document.querySelector("#img-select");
 const imgPreview = document.querySelector(".preview-img");
 
+if(!window.EyeDropper){
+   alert("Your browser does not support this feature") 
+}
+
+const pickerBtn = document.querySelector('.open-picker')
+const EyeDropper = new EyeDropper()
+
+
 imgInput.addEventListener('change', function(){
     const file = this.files[0];
     if (!file) return
@@ -11,4 +19,16 @@ imgInput.addEventListener('change', function(){
     })
 
     reader.readAsDataURL(file)
+})
+
+pickerBtn.addEventListener('click', function() {
+    eyeDropper.open()
+        .then(res => {
+            document.querySelector('.res').innerHTML = `Picked Color: <b>${res.sRGBHex}</b>`
+
+
+        })
+        .catch(err => {
+            console.log("User canceled the selection.");
+        })
 })
